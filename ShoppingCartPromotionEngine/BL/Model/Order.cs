a0b238aab280dfbbda1faec75ee407247c67edcd
sagartaralekar <sagartaralekar@gmail.com>
2020-08-27 10:06:44 +0530

    Added model

---
+++ ShoppingCartPromotionEngine/BL/Model/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Model
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public List<Product> Products { get; set; }
+
+        public Order(int _oid, List<Product> _prods)
+        {
+            this.OrderID = _oid;
+            this.Products = _prods;
+        }
+    }
+}
