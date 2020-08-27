2ea4bf7df06d3bf3d2297f415b76cec994746f3e
sagartaralekar <sagartaralekar@gmail.com>
2020-08-27 10:41:34 +0530

    corrected logic and implemented 2 more unit test case

--- ShoppingCartPromotionEngine/BL/Order.cs
+++ ShoppingCartPromotionEngine/BL/Order.cs
@@ -49,9 +49,23 @@ namespace BL.Model
             }
             int totalPriceofA = (counterofA / 3) * 130 + (counterofA % 3 * priceofA);
             int totalPriceofB = (counterofB / 2) * 45 + (counterofB % 2 * priceofB);
+
+
+            int totalPriceofCD = 0; 
+
+            if (CounterofC>0 && CounterofD>0)
+            {
+                while (CounterofC > 0 && CounterofD > 0)
+                {
+                    totalPriceofCD += 30;
+                    CounterofC--;
+                    CounterofD--;
+                }
+            }
+
             int totalPriceofC = (CounterofC * priceofC);
             int totalPriceofD = (CounterofD * priceofD);
-            return totalPriceofA + totalPriceofB + totalPriceofC + totalPriceofD;
+            return totalPriceofA + totalPriceofB + totalPriceofC + totalPriceofD+ totalPriceofCD;
 
         }
     }
--- ShoppingCartPromotionEngine/Test/UnitTest1.cs
+++ ShoppingCartPromotionEngine/Test/UnitTest1.cs
@@ -21,5 +21,48 @@ namespace Test
 
             Assert.AreEqual(totalPrice, 100);
         }
+
+        [TestMethod]
+        public void ScenarioB()
+        {
+            var order = new Order(1, new List<Product>() {
+                new Product("A"),
+                new Product("A"),
+                new Product("A"),
+                new Product("A"),
+                new Product("A"),
+                new Product("B"),
+                new Product("B"),
+                new Product("B"),
+                new Product("B"),
+                new Product("B"),
+                new Product("C")
+            });
+
+            var totalPrice = order.GetTotalPrice();
+
+            Assert.AreEqual(totalPrice, 370);
+        }
+
+        [TestMethod]
+        public void ScenarioC()
+        {
+            var order = new Order(1, new List<Product>() {
+                new Product("A"),
+                new Product("A"),
+                new Product("A"),
+                new Product("B"),
+                new Product("B"),
+                new Product("B"),
+                new Product("B"),
+                new Product("B"),
+                new Product("C"),
+                new Product("D")
+            });
+
+            var totalPrice = order.GetTotalPrice();
+
+            Assert.AreEqual(totalPrice, 280);
+        }
     }
 }
