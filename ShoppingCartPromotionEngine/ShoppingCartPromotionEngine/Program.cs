c78d722819eee1dc2b5620142ae280c14d298e98
sagartaralekar <sagartaralekar@gmail.com>
2020-08-27 13:01:31 +0530

    renamed test class and added code in console project

--- ShoppingCartPromotionEngine/ShoppingCartPromotionEngine/Program.cs
+++ ShoppingCartPromotionEngine/ShoppingCartPromotionEngine/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,23 @@ namespace ShoppingCartPromotionEngine
     {
         static void Main(string[] args)
         {
+            try
+            {
+                var order = new Order(1, new List<Product>() {
+                new Product("A"),
+                new Product("B"),
+                new Product("C")
+            });
+
+                var totalPrice = order.GetTotalPrice();
+                Console.WriteLine("Total Price of Order ="+ totalPrice);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }
--- ShoppingCartPromotionEngine/Test/UnitTestPromotionEngine.cs
+++ ShoppingCartPromotionEngine/Test/UnitTestPromotionEngine.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 namespace Test
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTestPromotionEngine
     {
         [TestMethod]
         public void ScenarioA()
